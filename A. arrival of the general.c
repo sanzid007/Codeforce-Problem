6cc26612f896b939d16f6f4384ade0b9249a2bee
sanzid007 <88428850+sanzid007@users.noreply.github.com>
2021-08-13 00:43:04 +0600

    Add files via upload

---
+++ A. arrival of the general.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,arr[100],max=0,min=99;
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+
+    }
+
+
+
+    return 0;
+}
