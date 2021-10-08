58f22bd4665081710da59c4f0612af87760fb176
sanzid007 <88428850+sanzid007@users.noreply.github.com>
2021-10-09 00:29:09 +0600

    Add files via upload

---
+++ A. word capitalizaiton.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    if(str[0] >= 97 && str[0] <= 122)
+        str[0] -= 32;
+
+    cout << str << endl;
+    return 0;
+}
+
